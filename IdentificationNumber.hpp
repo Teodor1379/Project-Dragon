e3f6a2a51799d1af1f4819f67176ac0ee21891f0
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-07-07 15:10:39 +0300

    Commit: Source - Identification Number

---
+++ IdentificationNumber.hpp
@@ -0,0 +1,30 @@
+#ifndef IDENTIFICATION_NUMBER_HPP_INCLUDED
+#define IDENTIFICATION_NUMBER_HPP_INCLUDED
+
+#include <cstdint>
+
+class IdentificationNumber {
+public:
+    IdentificationNumber();
+
+    inline bool getGender() const noexcept  { return this->gender; }
+
+    inline std::uint16_t getBirthDay()      const noexcept  { return this->birthDay;    }
+    inline std::uint16_t getBirthMonth()    const noexcept  { return this->birthMonth;  }
+    inline std::uint16_t getBirthYear()     const noexcept  { return this->birthYear;   }
+
+    inline std::uint8_t getBirthLocationLowerBorder()   const noexcept  { return this->birthLocationLowerBorder; }
+    inline std::uint8_t getBirthLocationUpperBorder()   const noexcept  { return this->birthLocationUpperBorder; }
+
+private:
+    bool gender;
+
+    std::uint16_t birthDay;
+    std::uint16_t birthMonth;
+    std::uint16_t birthYear;
+
+    std::uint8_t birthLocationLowerBorder;
+    std::uint8_t birthLocationUpperBorder;
+};
+
+#endif
