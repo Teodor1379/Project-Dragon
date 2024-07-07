7b60a3a530dfd81f0c1b2b1ec9936448fb06815e
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-07-07 15:07:17 +0300

    Commit: Source - Identification Number Container

---
+++ IdentificationNumberContainer.cpp
@@ -0,0 +1,33 @@
+#include "IdentificationNumberContainer.hpp"
+
+IdentificationNumberContainer::IdentificationNumberContainer() {
+    this->table["Благоевград"]      = { 0,      43  };
+    this->table["Бургас"]           = { 44,     93  };
+    this->table["Варна"]            = { 94,     139 };
+    this->table["Велико Търново"]   = { 140,    169 };
+    this->table["Видин"]            = { 170,    183 };
+    this->table["Враца"]            = { 184,    217 };
+    this->table["Габрово"]          = { 218,    233 };
+    this->table["Кърджали"]         = { 234,    281 };
+    this->table["Кюстендил"]        = { 282,    301 };
+    this->table["Ловеч"]            = { 302,    319 };
+    this->table["Монтана"]          = { 320,    341 };
+    this->table["Пазарджик"]        = { 342,    377 };
+    this->table["Перник"]           = { 378,    395 };
+    this->table["Плевен"]           = { 396,    435 };
+    this->table["Пловдив"]          = { 436,    501 };
+    this->table["Разград"]          = { 502,    527 };
+    this->table["Русе"]             = { 528,    555 };
+    this->table["Силистра"]         = { 556,    575 };
+    this->table["Сливен"]           = { 576,    601 };
+    this->table["Смолян"]           = { 602,    623 };
+    this->table["София-град"]       = { 624,    721 };
+    this->table["София-област"]     = { 722,    751 };
+    this->table["Стара Загора"]     = { 752,    789 };
+    this->table["Добрич"]           = { 790,    821 };
+    this->table["Търговище"]        = { 822,    843 };
+    this->table["Хасково"]          = { 844,    871 };
+    this->table["Шумен"]            = { 872,    903 };
+    this->table["Ямбол"]            = { 904,    925 };
+    this->table["Неизвестен"]       = { 926,    999};
+}
---
+++ IdentificationNumberContainer.hpp
@@ -0,0 +1,20 @@
+#ifndef IDENTIFICATION_NUMBER_CONTAINER_HPP_INCLUDED
+#define IDENTIFICATION_NUMBER_CONTAINER_HPP_INCLUDED
+
+#include <cstdint>
+
+#include <string>
+
+#include <unordered_map>
+
+typedef std::pair<std::uint8_t, std::uint8_t> Pair;
+
+class IdentificationNumberContainer {
+public:
+    IdentificationNumberContainer();
+
+private:
+    std::unordered_map<std::string, Pair> table;
+};
+
+#endif
