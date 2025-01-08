28f847270f5dbeff8136a75c2df33b553534a13e
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-01-08 19:32:54 +0200

    Commit: Source - Identification Number

---
+++ IdentificationNumber.cpp
@@ -0,0 +1,14 @@
+#include "IdentificationNumber.hpp"
+
+
+
+IdentificationNumber::IdentificationNumber() {
+    this->gender    =   0;
+    
+    this->birthDay      =   0;
+    this->birthMonth    =   0;
+    this->birthYear     =   0;
+
+    this->birthLocationLowerBorder  =   0;
+    this->birthLocationUpperBorder  =   0;  
+}
